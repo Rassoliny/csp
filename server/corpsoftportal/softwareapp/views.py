1c04ab2a780cf82cfe99b80ba954e5234873cad6
Rassoliny <dimo120@yandex.ru>
2019-01-24 23:40:33 +0300

    Category creation form is working

--- server/corpsoftportal/softwareapp/views.py
+++ server/corpsoftportal/softwareapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from softwareapp.models import Category
+from softwareapp.models import Category, LicenseType
 from softwareapp.forms import CategoryCreateForm, SofwareCreateForm
 
 # Create your views here.
@@ -14,14 +14,16 @@ def catalog_soft(request):
     query = Category.objects.all()
     return render(request, 'softwareapp/catalog_soft.html', {'results': query})
 
-#
+
 def category_create(request):
     title = 'Создание категории'
     # Вывод формы для редактирования
     if request.method == "POST":
         form = CategoryCreateForm(request.POST)
         if form.is_valid():
-            pass
+            category = Category(name=request.POST['name'], license_type=LicenseType.objects.get(id=request.POST['license_type']))
+            category.save()
+            return HttpResponseRedirect(reverse('softwareapp:main'))
     else:
         form = CategoryCreateForm()
 
@@ -29,13 +31,6 @@ def category_create(request):
         'title': title,
         'form': form
     }
-    # creation_form = CategoryCreateForm(data=request.POST or None)
-    #
-    # if request.method == 'POST' and creation_form.is_valid():
-    #     name = request.POST['name']
-    #     license_type = request.POST['license_type']
-    #
-    # content = {'title': title, 'add_form': creation_form}
 
     return render(request, 'softwareapp/category_creation.html', content)
 
