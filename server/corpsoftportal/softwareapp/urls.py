6e93e288268a40b79787c635c1a472e9e6432273
Rassoliny <dimo120@yandex.ru>
2019-01-24 16:53:14 +0300

    Category creation form

--- server/corpsoftportal/softwareapp/urls.py
+++ server/corpsoftportal/softwareapp/urls.py
@@ -5,6 +5,8 @@ app_name = 'softwareapp'
 
 urlpatterns = [
     path('catalog_soft', sofwareapp.catalog_soft, name='catalog_soft'),
-    path('category_creation', sofwareapp.catalog_soft, name='category_creation'),
+    # path('category_creation', sofwareapp.catalog_soft, name='category_creation'),
+    path('category_creation', sofwareapp.category_create, name='category_creation'),
+
     path('', sofwareapp.main, name='main'),
 ]
