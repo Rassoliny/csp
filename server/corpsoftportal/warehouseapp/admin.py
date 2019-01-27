28fe99309e7a7352640f9b7db346fe6c01cfc3b0
Rassoliny <dimo120@yandex.ru>
2019-01-27 16:23:16 +0300

    Warehouse model

--- server/corpsoftportal/warehouseapp/admin.py
+++ server/corpsoftportal/warehouseapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from warehouseapp.models import Warehouse
 # Register your models here.
+
+
+admin.site.register(Warehouse)
--- server/corpsoftportal/warehouseapp/models.py
+++ server/corpsoftportal/warehouseapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Warehouse(models.Model):
+    """class license type"""
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name = "Склад"
+        verbose_name_plural = "Склады"
+
+    def __str__(self):
+        return self.name
+
